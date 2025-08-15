a1f698f6deafc6a3029895f17a74516e58ecaf9a
ChaoticChrono <ChaoticChrono@proton.me>
2025-08-15 15:05:49 +0530

    added ghostty.nix

--- ptyxis.nix
+++ ptyxis.nix
@@ -1,26 +1,37 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    ptyxis
+  nixpkgs.overlays = [
+    (final: prev: {
+      gnome-terminal = prev.symlinkJoin {
+        name = "ptyxis-as-gnome-terminal";
+        paths = [ prev.ptyxis ];
+        postBuild = ''
+          # Binary override
+          mkdir -p $out/bin
+          ln -sf ${prev.ptyxis}/bin/ptyxis $out/bin/gnome-terminal
+
+          # Desktop entry override
+          mkdir -p $out/share/applications
+          cp ${prev.ptyxis}/share/applications/com.mitchellh.Ptyxis.desktop \
+             $out/share/applications/org.gnome.Terminal.desktop
+
+          # Fix the desktop entry name to say GNOME Terminal if you want to trick apps
+          sed -i 's/^Name=.*/Name=GNOME Terminal (Ptyxis)/' \
+            $out/share/applications/org.gnome.Terminal.desktop
+        '';
+      };
+    })
   ];
 
-  # Replace gnome-terminal with ptyxis
-  environment.etc."alternatives/gnome-terminal".source = pkgs.writeShellScript "gnome-terminal-wrapper" ''
-    #!${pkgs.bash}/bin/bash
-    args=()
-    for arg in "$@"; do
-      args+=("$arg")
-    done
-    exec ${pkgs.ptyxis}/bin/ptyxis "\${args[@]}"
-  '';
-
-  # Override desktop entry
   environment.systemPackages = [
-    (pkgs.runCommand "ptyxis-as-gnome-terminal" { buildInputs = [ pkgs.coreutils ]; } ''
-      mkdir -p $out/share/applications
-      cp ${pkgs.ptyxis}/share/applications/com.mitchellh.ptyxis.desktop \
-         $out/share/applications/org.gnome.Terminal.desktop
-    '')
+    pkgs.gnome-terminal
   ];
+
+  # Set GNOME’s default terminal to ptyxis
+  services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
+    [org.gnome.desktop.default-applications.terminal]
+    exec='ptyxis'
+    exec-arg=''
+  '';
 }
