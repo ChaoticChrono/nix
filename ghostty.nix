acbfaca7388f9b3f9763080b38b7bfaf7d9755cc
ChaoticChrono <ChaoticChrono@proton.me>
2025-08-15 14:47:29 +0530

    added ghostty.nix

--- ghostty.nix
+++ ghostty.nix
@@ -7,23 +7,25 @@
         name = "ghostty-as-gnome-terminal";
         paths = [ prev.ghostty ];
         postBuild = ''
-          # Symlinks for compatibility
+          # Make sure bin directory exists
+          mkdir -p $out/bin
+
+          # Symlinks so commands work like gnome-terminal
           ln -s $out/bin/ghostty $out/bin/xterm
           ln -s $out/bin/ghostty $out/bin/x-terminal-emulator
           ln -s $out/bin/ghostty $out/bin/gnome-terminal
 
-          # Masquerade as GNOME Terminal desktop entry
+          # Replace GNOME Terminal desktop entry with Ghostty
           mkdir -p $out/share/applications
           cp ${prev.ghostty}/share/applications/ghostty.desktop \
              $out/share/applications/gnome-terminal.desktop
           sed -i 's/^Name=.*/Name=Terminal/' \
-            $out/share/applications/gnome-terminal.desktop
+             $out/share/applications/gnome-terminal.desktop
         '';
       };
     })
   ];
 
-  environment.systemPackages = [
-    pkgs.gnome-terminal
-  ];
+  # Install "gnome-terminal" (which is actually Ghostty now)
+  environment.systemPackages = [ pkgs.gnome-terminal ];
 }
