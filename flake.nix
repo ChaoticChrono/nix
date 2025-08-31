ddfc6806ef6bc8879e8c51c8b9b51adaf84c00ca
ChaoticChrono <ChaoticChrono@proton.me>
2025-08-31 09:51:32 +0530

    flake update

--- flake.nix
+++ flake.nix
@@ -255,8 +255,6 @@
                   enable32Bit = true;
                   extraPackages = with pkgs; [
                    intel-media-driver
-                   intel-vaapi-driver 
-                   libvdpau-va-gl
                    vpl-gpu-rt
                    vulkan-loader
                    vulkan-validation-layers
